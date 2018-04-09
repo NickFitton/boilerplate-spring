9f154878815bcc46fec44c521e764af3004aa694
nf-PostQuantum <nf@post-quantum.com>
2018-04-09 15:26:32 +0100

    Move security config to /config and add web config for cors

--- src/main/java/com/nfitton/demostructure/config/SecurityConfig.java
+++ src/main/java/com/nfitton/demostructure/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.nfitton.demostructure.security;
+package com.nfitton.demostructure.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
